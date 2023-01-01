48d8e32946ed1641ab2db2bff6b2ed4c5d36077d
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2023-01-01 22:30:15 +0000

    Add method to find the moss location inside of a rituals shape

--- src/main/java/io/github/sarahgreywolf/redsorcery/interfaces/IRitual.java
+++ src/main/java/io/github/sarahgreywolf/redsorcery/interfaces/IRitual.java
@@ -6,6 +6,7 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
+import io.github.sarahgreywolf.redsorcery.util.ShapePos;
 import net.kyori.adventure.text.Component;
 
 public interface IRitual {
@@ -23,5 +24,22 @@ public interface IRitual {
     String getPermission();
 
     String help();
+
+    // Find where the moss block is in the rituals shape
+    default ShapePos findMossLocation() {
+        char[][][] shape = this.getShape();
+        ShapePos shapePos = null;
+        search: for (int layer = 0; layer < shape.length; layer++) {
+            for (int z = 0; z < shape[layer].length; z++) {
+                for (int x = 0; x < shape[layer][z].length; x++) {
+                    if (shape[layer][z][x] == 'X' && shape[layer + 1][z][x] == 'Y') {
+                        shapePos = new ShapePos(x, z, layer);
+                        break search;
+                    }
+                }
+            }
+        }
+        return shapePos;
+    }
 
 }
---
+++ src/main/java/io/github/sarahgreywolf/redsorcery/util/ShapePos.java
@@ -0,0 +1,46 @@
+package io.github.sarahgreywolf.redsorcery.util;
+
+public class ShapePos {
+    private int X;
+    private int Z;
+    private int LAYER;
+
+    public ShapePos() {
+
+    }
+
+    public ShapePos(int x, int z, int layer) {
+        X = x;
+        Z = z;
+        LAYER = layer;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public void setZ(int z) {
+        Z = z;
+    }
+
+    public void setLayer(int layer) {
+        LAYER = layer;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getZ() {
+        return Z;
+    }
+
+    public int getLayer() {
+        return LAYER;
+    }
+
+    public boolean isZero() {
+        return X == 0 && Z == 0 && LAYER == 0;
+    }
+
+}
